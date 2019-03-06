3da89465db72cfa68ca33acee32e6ba272e0f71c
kawai.poon <kawai.poon@rakuten.com>
2019-03-06 18:41:35 +0900

    try to create new set of buttons, and call their own functions by onClick. but it seems that call methods by their name doesn't help... ??

--- popup.js
+++ popup.js
@@ -7,19 +7,60 @@
 let page = document.getElementById('buttonDiv');
 
 const kButtonColors = [document.body.style.backgroundColor, '#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
+const kButtons = [
+  {
+    'text': 'Clear Cookies',
+    'color': '#3aa757',
+    'action': 'clearCookies()',
+    'name': 'byeCookies',
+  },
+  // {
+  //   'text': 'Bet rpat03',
+  //   'color': '#e8453c',
+  // },
+  // {
+  //   'text': 'Check Lunch Menu',
+  //   'color': '#f9bb2d',
+  // },
+  // {
+  //   'text': 'Clear Cookies',
+  //   'color': '#4688f1',
+  // }
+]
+function constructOptions(kButtons) {
+  for (let item of kButtons) {
     let button = document.createElement('button');
-    button.style.backgroundColor = item;
+    button.style.backgroundColor = item.color;
+    button.text = item.text;
+    button.name = item.name
     button.addEventListener('click', function() {
        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + item + '";'});
+            // {code: 'alert("'+button.text+'");'});  // ok
+            {code: 'clearCookies();'});               // failed -,-
       });
     });
     page.appendChild(button);
   }
 }
-constructOptions(kButtonColors);
+
+constructOptions(kButtons);
+
+function clearCookies() {
+  alert('hello');
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+
+  alert('You have no cookie now :D');
+}
+
+function checkLunch() {
+  window.location.href = 'https://officerakuten.sharepoint.com/sites/GlobalPortal/SitePages/top.aspx';
+}
