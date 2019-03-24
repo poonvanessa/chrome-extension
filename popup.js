30bd6e3f1f887068e3db4a4dcd0ee4d791be578d
echelper@ust.hk <iamvanessapoon@gmail.com>
2019-03-25 00:07:40 +0900

    sample for removing cookies

--- popup.js
+++ popup.js
@@ -7,19 +7,45 @@
 let page = document.getElementById('buttonDiv');
 
 const kButtonColors = [document.body.style.backgroundColor, '#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+const kButtons = [
+  {
+    'text' : 'Clear Cookies',
+    'name' : 'clearCookies',
+  },
+  {
+    'text' : 'Home page',
+    'name' : 'goHome',
+  }
+]
+const redirectUrl = 'https://www.google.com';
 
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
+function constructOptions(kButtons) {
+  for (let item of kButtons) {
     let button = document.createElement('button');
-    button.style.backgroundColor = item;
+    button.textContent = item.text;
+    button.name = item.name;
     button.addEventListener('click', function() {
-       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        var url = new URL(tab.url);
+        var domain = url.hostname;
+
+        chrome.cookies.getAll({domain: domain}, function (cookies){
+          for(var i=0;i<cookies.length;i++){
+            // console.log('url: '+ url, 'name: ' + cookies[i].name);
+            chrome.cookies.remove({url:url.toString(), name:cookies[i].name});
+          }
+        });
+
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + item + '";'});
+            {code: 'window.location.href = "' + redirectUrl + '";'}
+          );
       });
     });
     page.appendChild(button);
   }
 }
-constructOptions(kButtonColors);
+
+constructOptions(kButtons);
