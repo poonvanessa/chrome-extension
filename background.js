24630b2c2d4cf9854570d88413b048fec7c9d467
kawai.poon <kawai.poon@rakuten.com>
2019-03-26 17:33:51 +0900

    tmp

--- background.js
+++ background.js
@@ -4,18 +4,19 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        // pageUrl: {hostEquals: 'keiba.rakuten.co.jp'},
-        pageUrl: {hostEquals: 'developer.chrome.com'},  // todo 
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+const urlRegEx = '(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&\'\\(\\)\\*\\+,;=.]+';
+
+chrome.runtime.onInstalled.addListener(function () {
+    // chrome.storage.sync.set({color: '#3aa757'}, function () {
+    //     console.log("The color is green.");
+    // });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {urlMatches: urlRegEx },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
--- popup.js
+++ popup.js
@@ -7,104 +7,143 @@
 let page = document.getElementById('buttonDiv');
 
 const kButtons = [
-  {
-    'text' : 'Login on Local',
-    'name' : 'clearCookies',
-    'class' : 'btn btn-info block',
-  },
-  {
-    'text' : 'Check Lunch',
-    'name' : 'checkDinner',
-    'class' : 'btn btn-info block',
-  }
+    {
+        'text': 'Login as rpat03',
+        'name': 'loginRpat03Local',
+        'class': 'btn btn-warning block',
+    },
+    {
+        'text': 'Check Lunch',
+        'name': 'checkMenu',
+        'class': 'btn btn-warning block',
+    },
+    {
+      'text': 'Clear Cookies',
+      'name': 'clearCookies',
+      'class': 'btn btn-warning block',
+    }
 ]
-const rapMenuUrl = 'https://getbootstrap.com/docs/4.0/layout/grid/';  // todo
-const setBetUserUrl = 'https://www.google.com'; // todo
-const cookieName = '_ga'; // todo
+const rapMenuUrl = 'https://officerakuten.sharepoint.com/sites/GlobalPortal/SitePages/top.aspx';  // todo
+const setBetUserUrl = 'https://local.bet.keiba.rakuten.co.jp/sample/samples/login'; // todo
+const cookieName = 'Rz'; // todo
 var redirectUrl = '';
 
 function constructOptions(kButtons) {
-  for (let item of kButtons) {
-    let button = document.createElement('button');
-    button.textContent = item.text;
-    button.name = item.name;
-    button.className  = item.class;
-    button.addEventListener('click', function() {
-      switch (button.name) 
-      {
-        case 'clearCookies':
-          clearCookies();
-          break;
-        case 'checkDinner':
-          checkDinner();
-          break;
-        default:
-          console.log('no such button');
-          break;
-      }
-      
-    });
-    page.appendChild(button);
-  }
+    for (let item of kButtons) {
+        let button = document.createElement('button');
+        button.textContent = item.text;
+        button.name = item.name;
+        button.className = item.class;
+        button.addEventListener('click', function () {
+            switch (button.name) {
+                case 'clearCookies':
+                    clearCookies();
+                    break;
+                case 'checkMenu':
+                    checkMenu();
+                    break;
+                default:
+                    console.log('no such button');
+                    break;
+            }
+
+        });
+        page.appendChild(button);
+    }
 }
+
 constructOptions(kButtons);
 
-function clearCookies() 
-{
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var tab = tabs[0];
-    var url = new URL(tab.url);
-    redirectUrl = url;
-    
-    chrome.tabs.executeScript(
-      tab[0],
-      {
-        code: 'performance.getEntriesByType("resource").map(e => e.name)',
-      }, data => {
-        if (chrome.runtime.lastError || !data || !data[0]) return;
-        const urls = data[0].map(url => url.split(/[#?]/)[0]);
-        const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
-        Promise.all(
-          uniqueUrls.map(url =>
-            new Promise(resolve => {
-              chrome.cookies.getAll({url}, resolve);
-            })
-          )
-        ).then(results => {
-          // convert the array of arrays into a deduplicated flat array of cookies
-          const cookies = [
-            ...new Map(
-              [].concat(...results)
-                .map(c => [JSON.stringify(c), c])
-            ).values()
-          ];
-      
-          // do something with the cookies here
-          cookies.forEach(function (cookie) {
-            if (cookie.name === cookieName) {
-              chrome.cookies.remove({url:url.toString(), name:cookie.name});
-              // console.log(cookie, url.toString());
-            }
-          })
-          // console.log(uniqueUrls, cookies);
-        });
-      });   
+function loginRpat03Local() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        var tab = tabs[0];
+        var url = new URL(tab.url);
+        // redirectUrl = url;
+        redirectUrl = 'https://local.my.keiba.rakuten.co.jp/contribute/premium_exchange';
 
-    chrome.tabs.executeScript({
-      code: 'window.location.href = "' + setBetUserUrl + '"; window.location.href = "' + redirectUrl + '";'
+        chrome.tabs.executeScript(
+            tab[0],
+            {
+                code: 'performance.getEntriesByType("resource").map(e => e.name)',
+            }, data => {
+                if (chrome.runtime.lastError || !data || !data[0]) return;
+                const urls = data[0].map(url => url.split(/[#?]/)[0]);
+                const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
+                Promise.all(
+                    uniqueUrls.map(url =>
+                        new Promise(resolve => {
+                            chrome.cookies.getAll({url}, resolve);
+                        })
+                    )
+                ).then(results => {
+                    // convert the array of arrays into a deduplicated flat array of cookies
+                    const cookies = [
+                        ...new Map(
+                            [].concat(...results)
+                                .map(c => [JSON.stringify(c), c])
+                        ).values()
+                    ];
+
+                    // do something with the cookies here
+                    cookies.forEach(function (cookie) {
+                        if (cookie.name === cookieName) {
+                            chrome.cookies.remove({url: url.toString(), name: cookie.name});
+                            // console.log(cookie, url.toString());
+                        }
+                    })
+                    // console.log(uniqueUrls, cookies);
+                });
+            });
+
+        chrome.tabs.executeScript(
+            {code: 'window.location.href = "' + setBetUserUrl + '";'}
+            // {code: 'window.location.href = "' + setBetUserUrl + '"; window.location.href = "' + redirectUrl + '";'}
+        );
     });
-  });
 }
 
-function checkDinner() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+function checkMenu() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'window.location.href = "' + rapMenuUrl + '";'}
+        );
+    });
+}
+function clearCookies() {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     var tab = tabs[0];
     var url = new URL(tab.url);
-    redirectUrl = url;
-    
+
     chrome.tabs.executeScript(
-      tab[0],
-      {code: 'window.location.href = "' + rapMenuUrl + '";'}
-    );
-  });
+        tab[0],
+        {
+            code: 'performance.getEntriesByType("resource").map(e => e.name)',
+        }, data => {
+            if (chrome.runtime.lastError || !data || !data[0]) return;
+            const urls = data[0].map(url => url.split(/[#?]/)[0]);
+            const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
+            Promise.all(
+                uniqueUrls.map(url =>
+                    new Promise(resolve => {
+                        chrome.cookies.getAll({url}, resolve);
+                    })
+                )
+            ).then(results => {
+                // convert the array of arrays into a deduplicated flat array of cookies
+                const cookies = [
+                    ...new Map(
+                        [].concat(...results)
+                            .map(c => [JSON.stringify(c), c])
+                    ).values()
+                ];
+
+                // do something with the cookies here
+                cookies.forEach(function (cookie) {
+                    chrome.cookies.remove({url: url.toString(), name: cookie.name});
+                })
+                // console.log(uniqueUrls, cookies);
+            });
+        });
+});
 }
